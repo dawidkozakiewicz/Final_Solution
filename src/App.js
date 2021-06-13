c9d051a3bcf2c7bf322dc9e821dbd7862a94d81a
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-06-13 19:40:56 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import Beginning from './Beginning'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Outcomings from './Outcomings'
 
 
 
 
 function App() {
-  function handleReset() {
-    window.location.reload()
-  }
   return (
-
-
-    <div className="Beginning" >
-      <a href="http://dawidkozakiewicz.com/" target="_blank"><div className="author">O AUTORZE</div></a>
-      <button className="reset" onClick={handleReset}>RESET</button>
-      <Beginning />
-    </div>
-
-
+    <Router>
+      <div className="Navbar">
+        <Link className="Link" to="/wychodzace">WYCHODZĄCE</Link>
+        <Link className="Link" to="/przychodzace">PRZYCHODZĄCE</Link>
+      </div>
+      <div className="Beginning" >
+        <a href="http://dawidkozakiewicz.com/" target="_blank"><div className="author">O AUTORZE</div></a>
+        <Link to="/"><button className="reset">RESET</button></Link>
+        <Switch>
+          <Route exact path="/">
+            <h1>Wybierz z menu typ połączenia :-)</h1>
+          </Route>
+          <Route path="/przychodzace">
+            <Beginning />
+          </Route>
+          <Route path="/wychodzace">
+            <Outcomings />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Outcomings.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Outcomings = () => {
+    return (
+        <div>
+            <h1>WYCHODZĄCE</h1>
+        </div>
+    )
+}
+
+export default Outcomings

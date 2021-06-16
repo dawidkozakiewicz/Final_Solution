ab8fe7aeebd7e96796fa2224b803e7a1b6e6db0c
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-06-16 20:14:51 +0200

    deleting react router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 import Beginning from './Beginning'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { useState } from 'react';
 import Outcomings from './Outcomings'
 
 
 
 
 function App() {
+  const [connectionType, setConnectionType] = useState(null)
+
   return (
-    <Router>
-      <div className="Navbar">
-        <Link className="Link" to="/wychodzace">WYCHODZĄCE</Link>
-        <Link className="Link" to="/przychodzace">PRZYCHODZĄCE</Link>
-      </div>
+    <>
       <div className="Beginning" >
         <a href="http://dawidkozakiewicz.com/" target="_blank"><div className="author">O AUTORZE</div></a>
-        <Link to="/"><button className="reset">RESET</button></Link>
-        <Switch>
-          <Route exact path="/">
-            <h1>Cześć! :-) Wybierz z menu typ połączenia :-)</h1>
-          </Route>
-          <Route path="/przychodzace">
-            <Beginning />
-          </Route>
-          <Route path="/wychodzace">
-            <Outcomings />
-          </Route>
-        </Switch>
+        <button onClick={() => setConnectionType(null)} className="reset">RESET</button>
+        <h1>Cześć! :-) Wybierz z menu typ połączenia :-)</h1>
+        <button onClick={() => setConnectionType('in')}>IN</button>
+        <button onClick={() => setConnectionType('out')}>OUT</button>
+        {connectionType === null ? (<></>) : connectionType === 'in' ? (<Beginning />) : (<Outcomings />)}
+
       </div>
-    </Router>
+    </>
   );
 }
 
